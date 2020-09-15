b196476e9f7c587b260859721b303eb31b58122b
Dullayut <theadfine@gmail.com>
2020-09-16 00:37:16 +0700

    initial commit

---
+++ src/exercisefive/ExerciseFive.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercisefive;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Au
+ */
+public class ExerciseFive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ExerciseFiveBean MySource = new ExerciseFiveBean();
+        Listener1 list1 = new Listener1();
+        Listener2 list2 = new Listener2();
+        MySource.addPropertyChangeListener(list1);
+        MySource.addPropertyChangeListener(list2);
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Score ");
+        String football = sc.nextLine();
+        
+        while(!football.isEmpty()) {
+            MySource.setScore(football);
+            System.out.print("Enter Score ");
+            football = sc.nextLine();
+        }
+    }
+    
+}
---
+++ src/exercisefive/Listener1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercisefive;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+
+/**
+ *
+ * @author Au
+ */
+public class Listener1 implements PropertyChangeListener {
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        System.out.println("live result: " + evt.getNewValue()); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
